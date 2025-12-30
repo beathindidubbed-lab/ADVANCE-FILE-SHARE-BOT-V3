3987be5d7028bfdf0b719c6764386ce148014d50
beathindidubbed-lab <beathindidubbed@gmail.com>
2025-12-30 09:24:39 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,10 +1,9 @@
 """
 Plugins module
 Pyrogram will auto-discover all .py files in this directory
+"""
 
 import logging
 
 logger = logging.getLogger(__name__)
 logger.info("Plugins directory ready for auto-discovery")
-
-"""
