d4408efded912af79af1528a36d76f9e4445b89a
beathindidubbed-lab <beathindidubbed@gmail.com>
2025-12-30 10:17:41 +0530

    Delete plugins directory

--- plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Plugins module
-Pyrogram will auto-discover all .py files in this directory
-"""
-
-import logging
-
-logger = logging.getLogger(__name__)
-logger.info("Plugins directory ready for auto-discovery")
